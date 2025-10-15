f4e315c23a25643771789286423006952d36beca
Rithik Mohaanprakaas <rithikmohaan@gmail.com>
2025-10-15 22:18:36 +0530

    Implement simple calculator in Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+        char operator;
+
+        System.out.println("===== SIMPLE CALCULATOR =====");
+        System.out.print("Enter first number: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = sc.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
